92fae60d3c9cb9bc5d4d474cc1c82c422caa5aea
Jonathan Cronin <cronin_jonathan@bah.com>
2016-09-20 16:03:25 -0400

    waits for device and changes text size

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
 
-var region = new ibeacon.Region({
-	uuid: '7b44b47b-52a1-5381-90c2-f09b6838c5d4'
-});
 
-ibeacon.startRangingBeaconsInRegion({
-	region: region,
-	didRangeBeacons: function(result) {
-		//console.log('I see ' + result.beacons.length + ' beacons');
-		$('#count').text(result.beacons.length);
-	}
-});
+
+
+document.addEventListener("deviceready", onDeviceReady, false);
+
+// PhoneGap is loaded and it is now safe to make calls PhoneGap methods
+//
+function onDeviceReady() {
+    // Now safe to use the PhoneGap API
+    
+	var region = new ibeacon.Region({
+		uuid: '7b44b47b-52a1-5381-90c2-f09b6838c5d4'
+	});
+
+	ibeacon.startRangingBeaconsInRegion({
+		region: region,
+		didRangeBeacons: function(result) {
+			//console.log('I see ' + result.beacons.length + ' beacons');
+			$('#count').text(result.beacons.length);
+		}
+	});
+}
+
